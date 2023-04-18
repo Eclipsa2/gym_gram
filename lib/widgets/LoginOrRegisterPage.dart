08bc2e24a7135f9d93047c22fa996d084990d1ea
Andrei Ilie <andreivirgil@hotmail.com>
2023-04-18 18:22:33 +0300

    Added Login and SignUp but with bugs

---
+++ lib/widgets/LoginOrRegisterPage.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:gym_gram/widgets/LoginPage.dart';
+import 'package:gym_gram/widgets/RegisterPage.dart';
+
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({super.key});
+
+  @override
+  State<LoginOrRegisterPage> createState() => _LoginOrRegisterPageState();
+}
+
+class _LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  //initially show login page:
+  bool showLoginPage = false;
+
+  //switch between login and signUp
+  void switchPages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+    showLoginPage = !showLoginPage;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(
+        onTap: switchPages,
+      );
+    } else {
+      return RegisterPage(
+        onTap: switchPages,
+      );
+    }
+  }
+}
